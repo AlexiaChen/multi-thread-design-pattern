17a1752ffa72f069b47ea31aa213c0a0e11edb9b
MathxH Chen <brainfvck@foxmail.com>
2019-11-11 00:04:07 +0800

    Update comment

--- src/Balking/Main.java
+++ src/Balking/Main.java
@@ -1,7 +1,7 @@
 package Balking;
 
 /*
-*  Balking模式与Guard Suspension模式一样，也存在condition_variable类似的操作。
+*  Balking模式与Guard Suspension模式一样，也存在condition_variable类似的操作(当然，也可以不存在cond var，不要拘泥形式)。
 *  但是在该模式中，如果条件不成立，则是立即中断处理，反之，Guard Suspension模式是等到可以开始运行处理的条件
 *  简而言之就是，不需要（条件没达到）就算了
 */
